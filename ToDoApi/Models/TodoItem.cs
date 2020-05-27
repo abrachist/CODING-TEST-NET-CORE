d8dea139309e38dca99bbb52f08089453c8cdb68
manharist <manharist@gmail.com>
2020-05-27 10:55:59 +0700

    Add project files.

---
+++ ToDoApi/Models/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public DateTime ExpiredDate { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int CompletePercentage { get; set; }
+        public string Status { get; set; }
+    }
+}
